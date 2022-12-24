a35cbd97379a8177a08e011b216054a5ef3d9668
Thenu Kaluarachchi <thenujaya123@gmail.com>
2022-12-24 14:59:51 +0000

    Init

---
+++ components/common/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+import * as S from './NavBar.styled'
+
+const NavBar = () => {
+    return (
+        <S.NavBarContainer>
+            
+        </S.NavBarContainer>
+    );
+}
+
+export default NavBar;
