7c98bb90d04b26724cab5d769ab80167fd8a0897
Thenu Kaluarachchi <thenujaya123@gmail.com>
2022-12-25 14:47:27 +0000

    Added drop down menu logic

--- components/common/AltNavBar/AltNavBar.js
+++ components/common/AltNavBar/AltNavBar.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import * as S from './AltNavBar.styled'
 import DropDown from './DropDown/DropDown';
 
@@ -7,14 +7,30 @@ const AltNavBar = () => {
     //Drop down menu logic
     const [showMenu, setShowMenu] = useState(false)
     const toggleMenu = (e) => {
-        setShowMenu((!showMenu))
+        (showMenu)
+            ? setShowMenu(false)
+            : setShowMenu(true)
     }
+    useEffect(() => {
+        const html = document.querySelector('html')
+        function checkDrop(e) {
+            var test = e.target.closest('#dropDownMenu')
+            console.log(e.target.id)
+            if (showMenu===true && test===null && e.target.id!='menuButton'){
+                setShowMenu(false)
+            }
+        }
+        html.addEventListener('click', checkDrop )
+        return function cleanup(){
+            html.removeEventListener('click', checkDrop)
+        }
+    })
     return (
         <S.AltNavBarContainer className='center'>
             <nav>
                 <h1>AETERNUS</h1>
                 <button className="menuIcon" onClick={(e)=> toggleMenu(e)}>
-                    <img src="/menuIcon.png" alt="" />
+                    <img src="/menuIcon.png" alt="" id='menuButton' />
                 </button>
                 {
                     (showMenu)
--- components/common/AltNavBar/DropDown/DropDown.js
+++ components/common/AltNavBar/DropDown/DropDown.js
@@ -2,13 +2,13 @@ import * as S from './DropDown.styled'
 
 const DropDown = () => {
     return (
-        <S.DropDownContainer>
+        <S.DropDownContainer id='dropDownMenu'>
             <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Records</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Find Us</a></li>
-                <li><a href="">Contact</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Records</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Find Us</a></li>
+                <li><a href="#">Contact</a></li>
             </ul>
         </S.DropDownContainer>
     );
