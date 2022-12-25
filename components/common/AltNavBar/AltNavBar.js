f2563fe8dc1acf825cf7b80f802a1af03388790a
Thenu Kaluarachchi <thenujaya123@gmail.com>
2022-12-25 12:15:46 +0000

    Added navbar switch logic

---
+++ components/common/AltNavBar/AltNavBar.js
@@ -0,0 +1,11 @@
+import * as S from './AltNavBar.styled'
+
+const AltNavBar = () => {
+    return (
+        <S.AltNavBarContainer className='center'>
+            
+        </S.AltNavBarContainer>
+    );
+}
+
+export default AltNavBar;
--- pages/index.js
+++ pages/index.js
@@ -8,14 +8,34 @@ import Facts from '../components/Facts/Facts'
 import Sponsors from '../components/Sponsors/Sponsors'
 import Goal from '../components/Goal/Goal'
 import Footer from '../components/common/Footer/Footer'
+import AltNavBar from '../components/common/AltNavBar/AltNavBar'
+import { useEffect, useState } from 'react'
 
 
 
 export default function Home() {
+  
+  const [useAltNav, setUseAltNav] = useState(null)
+  useEffect(()=>{
+    const smallDevice = window.matchMedia("(min-width: 750px)");
+    smallDevice.addListener(handleDeviceChange);
+    function handleDeviceChange(e) {
+      if (e.matches) setUseAltNav(false);
+      else setUseAltNav(true);
+    }
+    handleDeviceChange(smallDevice);
+  }, () =>{
+    window.removeEventListener(); 
+  })
+
   return (
     <>
       <S.indexContainer>
-        <NavBar/>
+        {
+          (useAltNav)
+            ? <AltNavBar/>
+            : <NavBar/>
+        }
         <Landing/>
         <About/>
         <Sponsors/>
